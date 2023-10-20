68cab9003036454125674efc56387c83e27daad5
HumbertoCreate <112645101+HumbertoCreate@users.noreply.github.com>
2023-10-20 14:08:25 -0600

    finalmente solo faltan errores

--- src/principal/interfaz.java
+++ src/principal/interfaz.java
@@ -5,18 +5,21 @@ import java.util.Stack;
 public class interfaz extends javax.swing.JFrame {
 
     NumeroLinea nl;
-    String lexicoAlfabeto = "0123456789+-/*=(),;. \nABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz", cadenaEntrada, temporalId;
-    String palabrasReservadas[] = {"int","float","char"}, terminalesNoterminaleSintactico[] = {"id","num","int","float","chart",",",";","+","-","*","/","(",")","=","$","P","Tipo","V","A","Exp","E","Term","T","F"};
+    String lexicoAlfabeto = "0123456789+-/*=(),;. \nABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz$", cadenaEntrada, temporalId;
+    String palabrasReservadas[] = {"int","float","char"}, terminalesNoterminaleSintactico[] = {"id","num","int","float","char",",",";","+","-","*","/","(",")","=","$","P","Tipo","V","A","Exp","E","Term","T","F"};
+    String noterminalProducciones [] = {"p'", "P", "P", "Tipo", "Tipo", "Tipo", "V", "V", "A", "Exp", "Exp", "Exp", "E", "E", "E", "Term", "T", "T", "T", "F", "F", "F"};
     Stack<String> pilaSintactica = new Stack<>();
+    boolean banProduccion = false;
     int estado = 0, saltoLinea = 1, estadoSintactico = 0, tokenEntrada = 0;
+    int producciones[] = {0, 6, 2, 2, 2, 2, 6, 4, 8, 6, 6, 4, 6, 6, 0, 4, 6, 6, 0, 2, 2, 6};
     int tablaLexico[][] = {
-        { 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 6, 6, 6, 6, 6, 6, 6,-1, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
-        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 6, 6,-1, 6, 6,-1, 6, 2, 0, 0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
-        { 2, 3, 3, 3, 3, 3, 3, 3, 3, 3,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
-        { 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6,-1, 6, 6,-1, 6,-1, 0, 0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 6, 6, 6, 6,-1, 6, 6,-1, 6, 2, 0, 0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
-        { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6,-1, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
-        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 0, 0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1}
+        { 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 6, 6, 6, 6, 6, 6, 6,-1, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7},
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 6, 6,-1, 6, 6,-1, 6, 2, 0, 0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 7},
+        { 2, 3, 3, 3, 3, 3, 3, 3, 3, 3,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 7},
+        { 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6,-1, 6, 6,-1, 6,-1, 0, 0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 7},
+        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 6, 6, 6, 6,-1, 6, 6,-1, 6, 2, 0, 0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 7},
+        { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6,-1, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7},
+        {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 0, 0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 7}
     };
     String tablaSintactica[][] = {
         { "I7", "-1", "I4", "I5", "I6", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "-1", "I1", "I2", "-1", "I3", "-1", "-1", "-1", "-1", "-1"},
@@ -79,9 +82,11 @@ public class interfaz extends javax.swing.JFrame {
 
         inRaiz = new javax.swing.JScrollPane();
         inTexto = new javax.swing.JTextArea();
-        jButton1 = new javax.swing.JButton();
+        correr = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea2 = new javax.swing.JTextArea();
+        vistaAnalizadorSintactico = new javax.swing.JTextArea();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        vistaAnalizadorLexico = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -89,11 +94,20 @@ public class interfaz extends javax.swing.JFrame {
         inTexto.setRows(5);
         inRaiz.setViewportView(inTexto);
 
-        jButton1.setText("jButton1");
+        correr.setText("Correr");
+        correr.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                correrActionPerformed(evt);
+            }
+        });
 
-        jTextArea2.setColumns(20);
-        jTextArea2.setRows(5);
-        jScrollPane2.setViewportView(jTextArea2);
+        vistaAnalizadorSintactico.setColumns(20);
+        vistaAnalizadorSintactico.setRows(5);
+        jScrollPane2.setViewportView(vistaAnalizadorSintactico);
+
+        vistaAnalizadorLexico.setColumns(20);
+        vistaAnalizadorLexico.setRows(5);
+        jScrollPane1.setViewportView(vistaAnalizadorLexico);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -101,32 +115,45 @@ public class interfaz extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addComponent(correr)
                 .addGap(51, 51, 51))
             .addGroup(layout.createSequentialGroup()
                 .addGap(50, 50, 50)
                 .addComponent(inRaiz, javax.swing.GroupLayout.PREFERRED_SIZE, 411, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 197, Short.MAX_VALUE)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 382, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 382, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1))
                 .addGap(40, 40, 40))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(78, 78, 78)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(inRaiz, javax.swing.GroupLayout.PREFERRED_SIZE, 388, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(33, 33, 33)
+                        .addComponent(jScrollPane1)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 53, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addComponent(correr)
                 .addGap(32, 32, 32))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void correrActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_correrActionPerformed
+        this.IniciolizarVariables();
+        this.AnalizadorLexico();
+    }//GEN-LAST:event_correrActionPerformed
     
     public void IniciolizarVariables()
     {
+        temporalId = "";
+        cadenaEntrada = inTexto.getText();
+        cadenaEntrada += "$";
         pilaSintactica.push("$");
         pilaSintactica.push("I0");
     }
@@ -156,8 +183,8 @@ public class interfaz extends javax.swing.JFrame {
                 }
                 else
                     temporalId += cadenaEntrada.charAt(i);
-            if(i + 1 < cadenaEntrada.length() && lexicoAlfabeto.indexOf(cadenaEntrada.charAt(i)) != -1 )
-                if(tablaLexico[estado][lexicoAlfabeto.indexOf(cadenaEntrada.charAt(i +1))] == 0 || tablaLexico[estado][lexicoAlfabeto.indexOf(cadenaEntrada.charAt(i +1))] == 6)
+            if(i + 1 < cadenaEntrada.length() && lexicoAlfabeto.indexOf(cadenaEntrada.charAt(i + 1)) != -1 )
+                if(tablaLexico[estado][lexicoAlfabeto.indexOf(cadenaEntrada.charAt(i + 1))] == 0 || tablaLexico[estado][lexicoAlfabeto.indexOf(cadenaEntrada.charAt(i +1))] == 6)
                     this.ComprobarEstados(i);
                 else
                     if(estado == 6)
@@ -174,10 +201,10 @@ public class interfaz extends javax.swing.JFrame {
             case 1:
                 this.EnviarToken("num");
                 break;
-            case 4:
+            case 3:
                 this.EnviarToken("num");
                 break;
-            case 3:
+            case 4:
                 this.EnviarToken("num");
                 break;
             case 5:
@@ -186,6 +213,8 @@ public class interfaz extends javax.swing.JFrame {
             case 6:
                 this.EnviarToken(cadenaEntrada.charAt(i) + "");
                 break;
+            case 7:
+                this.TerminadorCadena(i);
         }
         
     }
@@ -203,47 +232,70 @@ public class interfaz extends javax.swing.JFrame {
             this.EnviarToken("id");
     }
     
+    public void TerminadorCadena(int i)
+    {
+        if(i == cadenaEntrada.length() - 1)
+            this.EnviarToken("$");
+        else
+            this.Error();
+    }
+    
     public void EnviarToken(String token)
     {
+        vistaAnalizadorLexico.append(token + "\n");
         this.AnalizadorSintactico(token);
     }
     
     public void AnalizadorSintactico(String token)
     {
-        boolean banProduccion = false;
-        
         do
         {
-            if(pilaSintactica.peek().charAt(1) == 'I')
+            if(pilaSintactica.peek().charAt(0) == 'I')
                 this.EstadosSintacticos(token);
             else
-                if(pilaSintactica.peek().charAt(1) == 'P' )
+                if(pilaSintactica.peek().charAt(0) == 'P' )
                     this.Producciones(pilaSintactica.peek());
                 else
                     this.Error();
+            
         }while(banProduccion);        
     }
     
     public void EstadosSintacticos(String token)
     {
         estadoSintactico = Integer.parseInt(pilaSintactica.peek().substring(1));
-        for(tokenEntrada = 0; tokenEntrada < terminalesNoterminaleSintactico.length && terminalesNoterminaleSintactico[tokenEntrada].equals(token); tokenEntrada++);
+        for(tokenEntrada = 0; tokenEntrada < terminalesNoterminaleSintactico.length && !terminalesNoterminaleSintactico[tokenEntrada].equals(token); tokenEntrada++);
         
-        if(tablaSintactica[estadoSintactico][tokenEntrada].charAt(1) == 'I')
+        if(tablaSintactica[estadoSintactico][tokenEntrada].charAt(0) == 'I')
         {
             pilaSintactica.push(token);
             pilaSintactica.push(tablaSintactica[estadoSintactico][tokenEntrada]);
+            banProduccion = false;
+            vistaAnalizadorSintactico.append(String.valueOf(pilaSintactica) + "\n");
         }
         else
-            if(tablaSintactica[estadoSintactico][tokenEntrada].charAt(1) == 'P')
+            if(tablaSintactica[estadoSintactico][tokenEntrada].charAt(0) == 'P')
                 this.Producciones(tablaSintactica[estadoSintactico][tokenEntrada]);
             else
                 this.Error();
     }
     
     public void Producciones(String produccion)
-    {
+     {
+        int nProduccion, nPops, x;
+        nProduccion = Integer.parseInt(produccion.substring(1));
+        nPops = producciones[nProduccion];
         
+        for(x = 0; x < nPops; x++)
+            pilaSintactica.pop();
+            
+        estadoSintactico = Integer.parseInt(pilaSintactica.peek().substring(1));
+        for(tokenEntrada = 0; tokenEntrada < terminalesNoterminaleSintactico.length && !terminalesNoterminaleSintactico[tokenEntrada].equals(noterminalProducciones[nProduccion]); tokenEntrada++);
+        
+        pilaSintactica.push(noterminalProducciones[nProduccion]);
+        pilaSintactica.push(tablaSintactica[estadoSintactico][tokenEntrada]);
+        vistaAnalizadorSintactico.append(String.valueOf(pilaSintactica) + "\n");
+        banProduccion = true;
     }
     
     public void Error()
@@ -277,10 +329,12 @@ public class interfaz extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton correr;
     private javax.swing.JScrollPane inRaiz;
     private javax.swing.JTextArea inTexto;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JTextArea vistaAnalizadorLexico;
+    private javax.swing.JTextArea vistaAnalizadorSintactico;
     // End of variables declaration//GEN-END:variables
 }
